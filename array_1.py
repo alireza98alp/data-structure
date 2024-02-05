5517cd04ecb2b3fc464d16ab90e2973039b049c1
Alireza <alireza77alp@gmail.com>
2024-02-05 20:17:46 +0330

    making arr and nitem private

--- array_1.py
+++ array_1.py
@@ -1,32 +1,32 @@
 class array():
     def __init__(self, maxsize: int):
         self.maxsize = maxsize
-        self.arr = [None for _ in range(self.maxsize)]
-        self.nitem = 0
+        self.__arr = [None for _ in range(self.maxsize)]
+        self.__nitem = 0
 
     def __contains__(self, item):
-        for i in range(self.nitem):
-            if self.arr[i] == item:
+        for i in range(self.__nitem):
+            if self.__arr[i] == item:
                 return True
         return False
 
     def append(self, item):
-        if self.nitem != self.maxsize:
-            self.arr[self.nitem] = item
-            self.nitem += 1
+        if self.__nitem != self.maxsize:
+            self.__arr[self.__nitem] = item
+            self.__nitem += 1
         else:
             print('full')
 
     def delete(self, item):
-        for i in range(self.nitem):
-            if self.arr[i] == item:
-                self.nitem -= 1
-                for j in range(i, self.nitem):
-                    self.arr[j] = self.arr[j+1]
+        for i in range(self.__nitem):
+            if self.__arr[i] == item:
+                self.__nitem -= 1
+                for j in range(i, self.__nitem):
+                    self.__arr[j] = self.__arr[j+1]
 
     def __repr__(self) -> str:
         s = '['
-        for i in range(self.nitem):
-            s += str(self.arr[i])+', '
+        for i in range(self.__nitem):
+            s += str(self.__arr[i])+', '
         s += '\b\b]'
         return s
