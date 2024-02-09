9145875197e0cae61ed4abefb012c0c6224e2e12
Alireza <alireza77alp@gmail.com>
2024-02-09 12:20:13 +0330

    array atributes changed to protected

--- array_1.py
+++ array_1.py
@@ -1,35 +1,38 @@
 class array():
     def __init__(self, maxsize: int):
         self.maxsize = maxsize
-        self.__arr = [None for _ in range(self.maxsize)]
-        self.__nitem = 0
+        self._arr = [None for _ in range(self.maxsize)]
+        self._nitem = 0
 
     def __contains__(self, item):
-        for i in range(self.__nitem):
-            if self.__arr[i] == item:
+        for i in range(self._nitem):
+            if self._arr[i] == item:
                 return True
         return False
 
     def append(self, item):
-        if self.__nitem != self.maxsize:
-            self.__arr[self.__nitem] = item
-            self.__nitem += 1
+        if self._nitem != self.maxsize:
+            self._arr[self._nitem] = item
+            self._nitem += 1
         else:
-            print('full')
+            raise ValueError('array is full')
 
     def delete(self, item):
-        for i in range(self.__nitem):
-            if self.__arr[i] == item:
-                self.__nitem -= 1
-                for j in range(i, self.__nitem):
-                    self.__arr[j] = self.__arr[j+1]
+        for i in range(self._nitem):
+            if self._arr[i] == item:
+                self._nitem -= 1
+                for j in range(i, self._nitem):
+                    self._arr[j] = self._arr[j+1]
 
     def __repr__(self) -> str:
-        s = '['
-        for i in range(self.__nitem):
-            s += str(self.__arr[i])+', '
-        s += '\b\b]'
-        return s
+        if self._nitem:
+            s = '['
+            for i in range(self._nitem):
+                s += str(self._arr[i])+', '
+                s += '\b\b]'
+            return s
+        else:
+            return '[ ]'
 
     def __len__(self) -> int:
-        return self.__nitem
+        return self._nitem
