043f1e74110be46f60faa24610496f8728f94db4
Alireza <alireza77alp@gmail.com>
2024-02-10 23:15:49 +0330

    insertSort difined

--- sort.py
+++ sort.py
@@ -15,3 +15,13 @@ def selectSort(L: List[int]):
             if L[j] < L[min]:
                 min = j
         L[i], L[min] = L[min], L[i]
+
+
+def insertSort(L: List[int]):
+    for i in range(1, len(L)):
+        tmp = L[i]
+        j = i
+        while tmp < L[j-1] and j > 0:
+            L[j] = L[j-1]
+            j -= 1
+        L[j] = tmp
