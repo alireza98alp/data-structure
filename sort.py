ad34667f699c592f3d2f5d839add202879f9c03b
Alireza <alireza77alp@gmail.com>
2024-02-10 19:03:06 +0330

    selectSort defined

--- sort.py
+++ sort.py
@@ -1,10 +1,17 @@
 from typing import List
-from random import randint
 
 
-def BubbleSort(L: List[int]) -> List[int]:
+def bubbleSort(L: List[int]):
     for outer in range(len(L), 0, -1):
         for i in range(outer-1):
             if L[i] > L[i+1]:
                 L[i], L[i+1] = L[i+1], L[i]
-    return L
+
+
+def selectSort(L: List[int]):
+    for i in range(len(L)):
+        min = i
+        for j in range(i, len(L)):
+            if L[j] < L[min]:
+                min = j
+        L[i], L[min] = L[min], L[i]
