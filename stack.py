09f6c2bb0940889ea8d4d18d30468f996c35e8ef
Alireza <alireza77alp@gmail.com>
2024-02-11 13:03:38 +0330

    stack created

---
+++ stack.py
@@ -0,0 +1,28 @@
+class Stack():
+    def __init__(self, maxsize):
+        self._nitem = 0
+        self._stack = [None for _ in range(maxsize)]
+        self._maxitem = maxsize
+
+    def pop(self):
+        if self._nitem == 0:
+            raise Exception('stack is empty')
+        else:
+            self._nitem -= 1
+            return self._stack[self._nitem+1]
+
+    def Push(self, item):
+        if self._nitem == self._maxitem:
+            raise Exception('stack is full')
+        else:
+
+            self._stack[self._nitem] = item
+            self._nitem += 1
+
+    def __len__(self):
+        return self._nitem
+
+
+s = Stack(12)
+for _ in range(13):
+    s.Push(_*5)
